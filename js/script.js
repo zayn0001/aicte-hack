fe56b9ad25ab52de9888e6368c0be2bb92da5a88
Mishal <mishal0404@gmail.com>
2023-06-21 00:35:40 +0530

    fixing data 2

--- js/script.js
+++ js/script.js
@@ -281,7 +281,7 @@ $(document).ready(function() {
   //console.log("asdasdad")
   $.ajax({
       type: "GET",
-      url: "aicte-hack/blob/main/data/data.csv",
+      url: "aicte-hack/data/data.csv",
       dataType: "text",
       success: function(data) {
           //console.log(data)
