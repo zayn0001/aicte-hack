c008c42c4713ff841d340fb95e30ee8765c8850e
Mishal <mishal0404@gmail.com>
2023-06-21 00:50:51 +0530

    fixx for sure

--- js/script.js
+++ js/script.js
@@ -281,7 +281,7 @@ $(document).ready(function() {
   //console.log("asdasdad")
   $.ajax({
       type: "GET",
-      url: "https://github.com/zayn0001/aicte-hack/blob/main/data/data.csv",
+      url: "data.csv",
       dataType: "text",
       success: function(data) {
           //console.log(data)
