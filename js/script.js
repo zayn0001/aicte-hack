3055294c5b5ed3dd9b66cf73d184a8349f139ef5
Mishal <mishal0404@gmail.com>
2023-06-21 01:06:22 +0530

    ntha work aavathe

--- js/script.js
+++ js/script.js
@@ -279,6 +279,8 @@ $(document).ready(function() {
 // When the user scrolls down 20px from the top of the document, show the button
    
   //console.log("asdasdad")
+
+  
   $.ajax({
       type: "GET",
       url: "js/data.csv",
